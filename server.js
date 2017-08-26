1bddf22f68bb00980a283286b6a94f413cd7c9b9
John Karahalis <john.karahalis@gmail.com>
2017-08-25 21:14:59 -0400

    Fix status message

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 
 const port = process.env.PORT || 3001;
 
-exec('./node_modules/.bin/json-server --watch --routes routes.json api.json --port ' + port, (error, stdout, stderr) => {
-    console.log(`Running json-server on port ${port}...`);
-});
+console.log(`Running json-server on port ${port}...`);
+execSync('./node_modules/.bin/json-server --watch --routes routes.json api.json --port ' + port);
