afca361176ab22457884e3a517cea35195948ba0
John Karahalis <john.karahalis@gmail.com>
2017-08-28 14:54:17 -0400

    Add comment about why server.js needs to exist

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const { execSync } = require('child_process');
 
 const port = process.env.PORT || 3001;
 
+// We could have run this in an npm script, but that wouldn't have given us
+// access to the value of process.env.PORT.
 console.log(`Running json-server on port ${port}...`);
 execSync('./node_modules/.bin/json-server --watch --read-only --routes routes.json api.json --port ' + port);
