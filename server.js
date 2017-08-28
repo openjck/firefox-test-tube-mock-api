644a5f50f3deae14ad36327caa99227783eeb6b8
John Karahalis <john.karahalis@gmail.com>
2017-08-28 14:53:31 -0400

    Make the API read-only

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const { execSync } = require('child_process');
 const port = process.env.PORT || 3001;
 
 console.log(`Running json-server on port ${port}...`);
-execSync('./node_modules/.bin/json-server --watch --routes routes.json api.json --port ' + port);
+execSync('./node_modules/.bin/json-server --watch --read-only --routes routes.json api.json --port ' + port);
